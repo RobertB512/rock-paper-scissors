1319fb200dd836a48e2707b2342107ce7fff1bb1
RobertB512 <rwbcool@gmail.com>
2022-12-14 11:42:53 -0800

    Define player choice, play one round of the game

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,48 @@
 // ----- steps to make the game -----
 // declare needed variables and functions
-    // make a function for the computer's choice
-    // make a variable for the player's choice
-    // make a round function to play a single round comparing the computer's and player's choices
     // make a function that will play multiple rounds
         // might be able to use a loop
-
+        
 // computer picks rock, paper, or scissors
 function getComputerChoice() {
   let ranNum = Math.floor(Math.random() * 3) + 1
   let computerChoice;
-  
+ 
   if (ranNum === 1) {
     computerChoice = "rock";
   } else if (ranNum === 2) {
     computerChoice = "paper";
   } else if (ranNum === 3) {
-    computerChoice = "scissors"
+    computerChoice = "scissors";
   } else {
-    console.log("computer could not pick a choice")
+    console.log("computer could not pick a choice");
   }
-  return computerChoice
+  return computerChoice;
 }
+
+// set player and computer choices
+let computerMove = getComputerChoice();
+let playerMove = "rock"; // this will eventually come from a choice made through the UI
+
+// play a round Rock Paper Scissors
+function gameRound(player, computer) {
+  // define parameters
+  player = playerMove.toLowerCase();
+  computer = computerMove;
+
+  // compare the player's and computer's choice
+  if (player === computer) {
+    console.log(`TIE: ${player} and ${computer} are even`);
+  } else if (player === "rock" && computer === "scissors") {
+    console.log(`player wins ${player} beats ${computer}`);
+  } else if (player === "paper" && computer === "rock") {
+    console.log(`player wins ${player} beats ${computer}`);
+  } else if (player === "scissors" && computer === "paper") {
+    console.log(`player wins ${player} beats ${computer}`);
+  } else {
+    console.log(`computer wins ${computer} beats ${player}`);
+  }
+}
+gameRound()
 
 
