4dc17dbfe943b438364c2b0c9e1870f64050becf
RobertB512 <rwbcool@gmail.com>
2023-01-04 14:26:41 -0800

    Add tie game over message, remove some comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,3 @@
-// TODO
-// - when a button is pressed, then player's image and computer's image are displayed and the game starts
-      // - a new round starts each time a button is pressed
-// - each round display a message saying who won the round and increases the corresponding score by 1
-// - when someone reaches a score of 5, display a message declaring who won and end the game so no further rounds can be played
-
-
 // select DOM elements
 let moveSelection = document.querySelectorAll(".choose-move");
 const showPlayerMove = document.querySelector(".show-player-move");
@@ -17,14 +10,17 @@ let playerMoveImg = document.createElement("img");
 computerMoveImg.classList.add("move-img");
 playerMoveImg.classList.add("move-img");
 
+// some variables
 let playerScore = 0;
 let computerScore = 0;
+
+// initialize scores to 0
 playerScoreBox.textContent = playerScore.toString();
 computerScoreBox.textContent = computerScore.toString();
 
+// get player's choice and play the game
 function getPlayerChoiceAndPlay() {
   let player;
-
   moveSelection.forEach(choice => choice.addEventListener("click", () => {
     if (choice.textContent?.toLowerCase() === "rock") {
       player = "rock";
@@ -37,12 +33,9 @@ function getPlayerChoiceAndPlay() {
       playerMoveImg.setAttribute("src", "./images/scissors.jpg");
     }
     showPlayerMove?.append(playerMoveImg);
-    // fullGame()
     let result = gameRound(player, getComputerChoice());
     fullGame(result)
-    
   }));
-  // return player;
 }
 
 // computer picks rock, paper, or scissors
@@ -68,9 +61,6 @@ function getComputerChoice() {
 
 // play a round of the game
 function gameRound(playerMove, computerMove) {
-  // parameters
-  // computerMove = getComputerChoice();
-  // playerMove = getPlayerChoice();
   console.log(playerMove);
   console.log(computerMove);
 
@@ -101,11 +91,6 @@ function gameRound(playerMove, computerMove) {
 let rounds = 0;
 // play several rounds of the game
 function fullGame(result) {
-  // let playerScore = 0;
-  // let computerScore = 0;
-  
-  // let rounds = 1;
-  // for (let rounds = 1; rounds <= 5; rounds++) {
     if (result === "player wins") {
       playerScore += 1;
       playerScoreBox.textContent = playerScore.toString();
@@ -113,6 +98,7 @@ function fullGame(result) {
       computerScore += 1;
       computerScoreBox.textContent = computerScore.toString();
     }
+
     rounds++;
     console.log(`player: ${playerScore}`);
     console.log(`computer: ${computerScore}`);
@@ -127,12 +113,10 @@ function fullGame(result) {
         gameMessage.textContent = "GAME OVER! Refresh the page to try again.";
       } else if (playerScore === computerScore) {
         console.log("TIE");
+        gameMessage.textContent = "TIE! Refresh the page to play again."
       }
       moveSelection.forEach(btn => btn.setAttribute("disabled", "disabled"));
     }    
-  // }
-  
- 
 }
 
 getPlayerChoiceAndPlay()
