d7876e0518544218895e2fb2ff64322a63759199
RobertB512 <rwbcool@gmail.com>
2023-01-03 16:28:32 -0800

    Show computer's move image

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
+// TODO
+// - click a button and have it's move img appear on in the player's play area
+  // - if rock, show a rock img
+  // - if paper, sow a paper img
+  // - if scissors, show a scissors img
+// - based on a random number the computer chooses, have it's move img appear in the computer's play area
+  // - if 1, show rock img
+  // - if 2 show paper img
+  // - if 3 scissors img
+// - each round display a message saying who won the round and increases the corresponding score by 1
+// - when someone reaches a score of 5, display a message declaring who won and end the game so no further rounds can be played
+
+
 // select DOM elements
 let moveSelection = document.querySelectorAll(".choose-move");
 const showPlayerMove = document.querySelector(".show-player-move");
@@ -5,6 +18,8 @@ const showComputerMove = document.querySelector(".show-computer-move");
 let playerScoreBox = document.querySelector(".player-score-box");
 let computerScoreBox = document.querySelector(".computer-score-box");
 let gameMessage = document.querySelector(".game-message");
+let moveImg = document.createElement("img");
+moveImg.classList.add("move-img");
 
 
 // computer picks rock, paper, or scissors
@@ -14,20 +29,23 @@ function getComputerChoice() {
  
   if (ranNum === 1) {
     computerChoice = "rock";
+    moveImg.setAttribute("src", "./images/rocks.jpg");
   } else if (ranNum === 2) {
     computerChoice = "paper";
+    moveImg.setAttribute("src", "./images/paper.jpg");
   } else if (ranNum === 3) {
     computerChoice = "scissors";
+    moveImg.setAttribute("src", "./images/scissors.jpg");
   } else {
     console.log("computer could not pick a choice");
   }
+  showComputerMove?.append(moveImg)
   return computerChoice;
 }
 
 // set player and computer choices
 let computerMove = getComputerChoice();
-let player = "rock"; // this will eventually come from a choice made through the UI
-
+// let player = "rock"; // this will eventually come from a choice made through the UI
 
 // play a round of the game
 function gameRound(playerMove, computerMove) {
